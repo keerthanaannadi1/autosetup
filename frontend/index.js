41d39fd9211d7fd76a49054c34cbbbfe40a8f524
AKeerthana <keerthiannadi5038@gmail.com>
2026-07-03 16:01:39 +0530

    added new changes

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,15 @@
-console.log("Hello from js");
-alert("Hello from js");
+//console.log("Hello from js");
+//alert("Hello from js");
+
+const  users = {
+    {name: "John Doe"}
+}
+let currUserList = 0;
+function toggleUser() {
+    if(currUserList == 0) {
+     currUserList = 1;
+    }else {
+      currUserList = 0;
+    }
+    
+}
